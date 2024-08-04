63fe1f95cffe344c0876a51d5d98e17983ffa511
ken-delantar <kenjayson83@gmail.com>
2024-08-05 01:42:24 +0800

    try

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+// Allow requests from the specific origin
+header('Access-Control-Allow-Origin: https://ken-delantar.github.io'); 
+// Allow all origins (for testing purposes)
+header('Access-Control-Allow-Origin: *');
+
+// Allow specific HTTP methods
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+// Allow specific headers
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Content-Type header
 header('Content-Type: application/json');
 
 // Sample user data
